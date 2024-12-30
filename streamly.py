5ebaf85ae87127796c3cef4aa1e254444609ecc8
Aditya Shukla <aditya.shukla@evontech.com>
2024-12-30 19:09:45 +0530

    PY-GPT first commit

--- streamly.py
+++ streamly.py
@@ -1,4 +1,3 @@
-import openai
 import streamlit as st
 import logging
 from PIL import Image, ImageEnhance
@@ -6,9 +5,6 @@ import time
 import json
 import requests
 import base64
-from openai import OpenAI, OpenAIError
-
-
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -18,14 +14,10 @@ NUMBER_OF_MESSAGES_TO_DISPLAY = 20
 API_DOCS_URL = "https://docs.streamlit.io/library/api-reference"
 
 # Retrieve and validate API key
-OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY", None)
-if not OPENAI_API_KEY:
-    st.error("Please add your OpenAI API key to the Streamlit secrets.toml file.")
+CUSTOM_API_KEY = st.secrets.get("CUSTOM_API_KEY", None)
+if not CUSTOM_API_KEY:
+    st.error("Please add your custom API key to the Streamlit secrets.toml file.")
     st.stop()
-
-# Assign OpenAI API Key
-openai.api_key = OPENAI_API_KEY
-client = openai.OpenAI()
 
 # Streamlit Page Configuration
 st.set_page_config(
@@ -37,21 +29,20 @@ st.set_page_config(
         "Get help": "https://github.com/AdieLaine/Streamly",
         "Report a bug": "https://github.com/AdieLaine/Streamly",
         "About": """
-            ## Streamly Streamlit Assistant
-            ### Powered using GPT-4o-mini
+            ## PY-GPT - An Intelligent Assistant
+            ### Powered by PYINDIA
 
             **GitHub**: https://github.com/AdieLaine/
 
-            The AI Assistant named, Streamly, aims to provide the latest updates from Streamlit,
+            The AI Assistant, Streamly, aims to provide the latest updates from Streamlit,
             generate code snippets for Streamlit widgets,
             and answer questions about Streamlit's latest features, issues, and more.
-            Streamly has been trained on the latest Streamlit updates and documentation.
         """
     }
 )
 
 # Streamlit Title
-st.title("Streamly Streamlit Assistant")
+st.title("PY-GPT - An Intelligent Assistant")
 
 def img_to_base64(image_path):
     """Convert image to base64."""
@@ -64,35 +55,9 @@ def img_to_base64(image_path):
 
 @st.cache_data(show_spinner=False)
 def long_running_task(duration):
-    """
-    Simulates a long-running operation.
-
-    Parameters:
-    - duration: int, duration of the task in seconds
-
-    Returns:
-    - str: Completion message
-    """
+    """Simulates a long-running operation."""
     time.sleep(duration)
     return "Long-running operation completed."
-
-@st.cache_data(show_spinner=False)
-def load_and_enhance_image(image_path, enhance=False):
-    """
-    Load and optionally enhance an image.
-
-    Parameters:
-    - image_path: str, path of the image
-    - enhance: bool, whether to enhance the image or not
-
-    Returns:
-    - img: PIL.Image.Image, (enhanced) image
-    """
-    img = Image.open(image_path)
-    if enhance:
-        enhancer = ImageEnhance.Contrast(img)
-        img = enhancer.enhance(1.8)
-    return img
 
 @st.cache_data(show_spinner=False)
 def load_streamlit_updates():
@@ -104,95 +69,19 @@ def load_streamlit_updates():
         logging.error(f"Error loading JSON: {str(e)}")
         return {}
 
-def get_streamlit_api_code_version():
-    """
-    Get the current Streamlit API code version from the Streamlit API documentation.
-
-    Returns:
-    - str: The current Streamlit API code version.
-    """
-    try:
-        response = requests.get(API_DOCS_URL)
-        if response.status_code == 200:
-            return "1.36"
-    except requests.exceptions.RequestException as e:
-        logging.error(f"Error connecting to the Streamlit API documentation: {str(e)}")
-    return None
-
-def display_streamlit_updates():
-    """Display the latest updates of the Streamlit."""
-    with st.expander("Streamlit 1.36 Announcement", expanded=False):
-        st.markdown("For more details on this version, check out the [Streamlit Forum post](https://docs.streamlit.io/library/changelog#version).")
-
 def initialize_conversation():
-    """
-    Initialize the conversation history with system and assistant messages.
-
-    Returns:
-    - list: Initialized conversation history.
-    """
+    """Initialize the conversation history."""
     assistant_message = "Hello! I am Streamly. How can I assist you with Streamlit today?"
-
     conversation_history = [
         {"role": "system", "content": "You are Streamly, a specialized AI assistant trained in Streamlit."},
-        {"role": "system", "content": "Streamly, is powered by the OpenAI GPT-4o-mini model, released on July 18, 2024."},
-        {"role": "system", "content": "You are trained up to Streamlit Version 1.36.0, release on June 20, 2024."},
-        {"role": "system", "content": "Refer to conversation history to provide context to your response."},
-        {"role": "system", "content": "You were created by Madie Laine, an OpenAI Researcher."},
         {"role": "assistant", "content": assistant_message}
     ]
     return conversation_history
 
 @st.cache_data(show_spinner=False)
-def get_latest_update_from_json(keyword, latest_updates):
-    """
-    Fetch the latest Streamlit update based on a keyword.
-
-    Parameters:
-    - keyword (str): The keyword to search for in the Streamlit updates.
-    - latest_updates (dict): The latest Streamlit updates data.
-
-    Returns:
-    - str: The latest update related to the keyword, or a message if no update is found.
-    """
-    for section in ["Highlights", "Notable Changes", "Other Changes"]:
-        for sub_key, sub_value in latest_updates.get(section, {}).items():
-            for key, value in sub_value.items():
-                if keyword.lower() in key.lower() or keyword.lower() in value.lower():
-                    return f"Section: {section}\nSub-Category: {sub_key}\n{key}: {value}"
-    return "No updates found for the specified keyword."
-
-def construct_formatted_message(latest_updates):
-    """
-    Construct formatted message for the latest updates.
-
-    Parameters:
-    - latest_updates (dict): The latest Streamlit updates data.
-
-    Returns:
-    - str: Formatted update messages.
-    """
-    formatted_message = []
-    highlights = latest_updates.get("Highlights", {})
-    version_info = highlights.get("Version 1.36", {})
-    if version_info:
-        description = version_info.get("Description", "No description available.")
-        formatted_message.append(f"- **Version 1.36**: {description}")
-
-    for category, updates in latest_updates.items():
-        formatted_message.append(f"**{category}**:")
-        for sub_key, sub_values in updates.items():
-            if sub_key != "Version 1.36":  # Skip the version info as it's already included
-                description = sub_values.get("Description", "No description available.")
-                documentation = sub_values.get("Documentation", "No documentation available.")
-                formatted_message.append(f"- **{sub_key}**: {description}")
-                formatted_message.append(f"  - **Documentation**: {documentation}")
-    return "\n".join(formatted_message)
-
-@st.cache_data(show_spinner=False)
 def on_chat_submit(chat_input, latest_updates):
     """
-    Handle chat input submissions and interact with the OpenAI API.
+    Handle chat input submissions and interact with the custom API.
 
     Parameters:
     - chat_input (str): The chat input from the user.
@@ -201,7 +90,7 @@ def on_chat_submit(chat_input, latest_updates):
     Returns:
     - None: Updates the chat history in Streamlit's session state.
     """
-    user_input = chat_input.strip().lower()
+    user_input = chat_input.strip()
 
     if 'conversation_history' not in st.session_state:
         st.session_state.conversation_history = initialize_conversation()
@@ -209,32 +98,40 @@ def on_chat_submit(chat_input, latest_updates):
     st.session_state.conversation_history.append({"role": "user", "content": user_input})
 
     try:
-        model_engine = "gpt-4o-mini"
-        assistant_reply = ""
+        # Replace OpenAI with your custom API
+        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={CUSTOM_API_KEY}"
+        payload = json.dumps({
+            "contents": [
+                {
+                    "parts": [
+                        {
+                            "text": user_input
+                        }
+                    ]
+                }
+            ]
+        })
+        
+        headers = {
+            'Content-Type': 'application/json'
+        }
 
-        if "latest updates" in user_input:
-            assistant_reply = "Here are the latest highlights from Streamlit:\n"
-            highlights = latest_updates.get("Highlights", {})
-            if highlights:
-                for version, info in highlights.items():
-                    description = info.get("Description", "No description available.")
-                    assistant_reply += f"- **{version}**: {description}\n"
-            else:
-                assistant_reply = "No highlights found."
+        response = requests.post(url, headers=headers, data=payload)
+        if response.status_code == 200:
+            # response_data = response.json()["candidates"][0]
+            assistant_reply = response.json()["candidates"][0]["content"]["parts"][0].get('text', "I couldn't process your request.")
+            # assistant_reply = response_data.get('contents')["parts"][0].get('text', "I couldn't process your request.")
         else:
-            response = client.chat.completions.create(
-                model=model_engine,
-                messages=st.session_state.conversation_history
-            )
-            assistant_reply = response.choices[0].message.content
+            logging.error(f"API request failed with status {response.status_code}: {response.text}")
+            assistant_reply = "I'm unable to fetch a response at the moment. Please try again later."
 
         st.session_state.conversation_history.append({"role": "assistant", "content": assistant_reply})
         st.session_state.history.append({"role": "user", "content": user_input})
         st.session_state.history.append({"role": "assistant", "content": assistant_reply})
 
-    except OpenAIError as e:
+    except Exception as e:
         logging.error(f"Error occurred: {e}")
-        st.error(f"OpenAI Error: {str(e)}")
+        st.error(f"Error: {str(e)}")
 
 def initialize_session_state():
     """Initialize session state variables."""
@@ -244,91 +141,20 @@ def initialize_session_state():
         st.session_state.conversation_history = []
 
 def main():
-    """
-    Display Streamlit updates and handle the chat interface.
-    """
+    """Display Streamlit updates and handle the chat interface."""
     initialize_session_state()
 
     if not st.session_state.history:
-        initial_bot_message = "Hello! How can I assist you with Streamlit today?"
+        initial_bot_message = "Hello! How can I assist you with PY-GPT today?"
         st.session_state.history.append({"role": "assistant", "content": initial_bot_message})
         st.session_state.conversation_history = initialize_conversation()
 
-    # Insert custom CSS for glowing effect
-    st.markdown(
-        """
-        <style>
-        .cover-glow {
-            width: 100%;
-            height: auto;
-            padding: 3px;
-            box-shadow: 
-                0 0 5px #330000,
-                0 0 10px #660000,
-                0 0 15px #990000,
-                0 0 20px #CC0000,
-                0 0 25px #FF0000,
-                0 0 30px #FF3333,
-                0 0 35px #FF6666;
-            position: relative;
-            z-index: -1;
-            border-radius: 45px;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
-
-    # Load and display sidebar image
-    img_path = "imgs/sidebar_streamly_avatar.png"
-    img_base64 = img_to_base64(img_path)
-    if img_base64:
-        st.sidebar.markdown(
-            f'<img src="data:image/png;base64,{img_base64}" class="cover-glow">',
-            unsafe_allow_html=True,
-        )
-
-    st.sidebar.markdown("---")
-
-    # Sidebar for Mode Selection
+    # Sidebar setup
+    st.sidebar.title("PY-GPT")
     mode = st.sidebar.radio("Select Mode:", options=["Latest Updates", "Chat with Streamly"], index=1)
 
-    st.sidebar.markdown("---")
-
-    # Display basic interactions
-    show_basic_info = st.sidebar.checkbox("Show Basic Interactions", value=True)
-    if show_basic_info:
-        st.sidebar.markdown("""
-        ### Basic Interactions
-        - **Ask About Streamlit**: Type your questions about Streamlit's latest updates, features, or issues.
-        - **Search for Code**: Use keywords like 'code example', 'syntax', or 'how-to' to get relevant code snippets.
-        - **Navigate Updates**: Switch to 'Updates' mode to browse the latest Streamlit updates in detail.
-        """)
-
-    # Display advanced interactions
-    show_advanced_info = st.sidebar.checkbox("Show Advanced Interactions", value=False)
-    if show_advanced_info:
-        st.sidebar.markdown("""
-        ### Advanced Interactions
-        - **Generate an App**: Use keywords like **generate app**, **create app** to get a basic Streamlit app code.
-        - **Code Explanation**: Ask for **code explanation**, **walk me through the code** to understand the underlying logic of Streamlit code snippets.
-        - **Project Analysis**: Use **analyze my project**, **technical feedback** to get insights and recommendations on your current Streamlit project.
-        - **Debug Assistance**: Use **debug this**, **fix this error** to get help with troubleshooting issues in your Streamlit app.
-        """)
-
-    st.sidebar.markdown("---")
-
-    # Load and display image with glowing effect
-    img_path = "imgs/stsidebarimg.png"
-    img_base64 = img_to_base64(img_path)
-    if img_base64:
-        st.sidebar.markdown(
-            f'<img src="data:image/png;base64,{img_base64}" class="cover-glow">',
-            unsafe_allow_html=True,
-        )
-
     if mode == "Chat with Streamly":
-        chat_input = st.chat_input("Ask me about Streamlit updates:")
+        chat_input = st.chat_input("Ask me about PY-GPT updates:")
         if chat_input:
             latest_updates = load_streamlit_updates()
             on_chat_submit(chat_input, latest_updates)
@@ -336,12 +162,47 @@ def main():
         # Display chat history
         for message in st.session_state.history[-NUMBER_OF_MESSAGES_TO_DISPLAY:]:
             role = message["role"]
-            avatar_image = "imgs/avatar_streamly.png" if role == "assistant" else "imgs/stuser.png" if role == "user" else None
+            avatar_image = "imgs/avatar_streamly.png" if role == "assistant" else "imgs/stuser.png"
             with st.chat_message(role, avatar=avatar_image):
                 st.write(message["content"])
 
     else:
-        display_streamlit_updates()
+        st.markdown(
+    """
+    <style>
+    .pyindia-section {
+        padding: 20px;
+        border-radius: 10px;
+        box-shadow: 2px 4px 10px rgba(0, 0, 0, 0.1);
+        font-family: 'Arial', sans-serif;
+    }
+    .pyindia-header {
+       
+        font-size: 28px;
+        font-weight: bold;
+        text-align: center;
+        margin-bottom: 10px;
+    }
+    .pyindia-content {
+       
+        font-size: 16px;
+        line-height: 1.6;
+        text-align: justify;
+    }
+    </style>
+
+    <div class="pyindia-section">
+        <div class="pyindia-header">PyIndia: Empowering Python and AI Enthusiasts Across India</div>
+        <div class="pyindia-content">
+            PyIndia is a dynamic community for Python developers, AI enthusiasts, and tech learners across India. It serves as a hub for knowledge sharing, collaboration, and fostering innovation in Python programming and artificial intelligence. Through engaging meetups, hackathons, and workshops, PyIndia explores Python’s applications in AI, data science, web development, and beyond. 
+            <br><br>
+            The community keeps members updated on cutting-edge AI advancements, tools, and trends, empowering them to stay ahead in the evolving tech landscape. By promoting open-source contributions, networking, and professional growth, PyIndia inspires creativity and drives Python and AI adoption nationwide. Join PyIndia and shape the future!
+        </div>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
 
 if __name__ == "__main__":
     main()
